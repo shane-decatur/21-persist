fa7fe742a63b87bd59c13d539bd4d3a821399d9c
shane-decatur <sdecatur20@stuy.edu>
2021-12-14 11:24:01 -0500

    added files

---
+++ pipe_networking.c
@@ -0,0 +1,61 @@
+#include "pipe_networking.h"
+
+
+/*=========================
+  server_handshake
+  args: int * to_client
+  Performs the client side pipe 3 way handshake.
+  Sets *to_client to the file descriptor to the downstream pipe.
+  returns the file descriptor for the upstream pipe.
+  =========================*/
+int server_handshake(int *to_client) {
+
+  mkfifo(WKP, 0644);
+  printf("WKP created\n");
+  int from_client = open(WKP, O_RDONLY);
+
+  char secret_name[HANDSHAKE_BUFFER_SIZE];
+  read(from_client,secret_name,HANDSHAKE_BUFFER_SIZE);
+  printf("server connected, secret name: %s\n",secret_name);
+  remove(WKP);
+
+  *to_client = open(secret_name,O_WRONLY);
+  printf("opened private server\n");
+  write(*to_client,ACK,strlen(ACK));
+
+  char message[HANDSHAKE_BUFFER_SIZE];
+  read(from_client,message,HANDSHAKE_BUFFER_SIZE);
+  printf("reponse received: %s\n",message);
+
+  return from_client;
+}
+
+
+/*=========================
+  client_handshake
+  args: int * to_server
+  Performs the client side pipe 3 way handshake.
+  Sets *to_server to the file descriptor for the upstream pipe.
+  returns the file descriptor for the downstream pipe.
+  =========================*/
+int client_handshake(int *to_server) {
+
+  char secret_name[BUFFER_SIZE];
+  sprintf(secret_name,"%d",getpid());
+  mkfifo(secret_name,0644);
+  printf("client created secret pipe\n");
+
+  *to_server = open(WKP, O_WRONLY);
+  write(*to_server, secret_name,strlen(secret_name));
+
+  char message[HANDSHAKE_BUFFER_SIZE];
+  int from_server = open(secret_name,O_RDONLY);
+  read(from_server,message,HANDSHAKE_BUFFER_SIZE);
+
+  printf("client got: %s\n",message);
+  remove(secret_name);
+
+  if (!strcmp(message,ACK)) write(*to_server, "success", strlen("success"));
+
+  return from_server;
+}
